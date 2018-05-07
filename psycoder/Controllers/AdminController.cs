a402a333767183a199060a8ebe3c22699aeb21de
jorzen <jorzen2010@163.com>
2018-05-07 16:24:45 +0800

    难点

---
+++ psycoder/Controllers/AdminController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace psycoder.Controllers
+{
+    public class AdminController : Controller
+    {
+        //
+        // GET: /Admin/
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult TuwenSucai()
+        {
+            return View();
+        }
+        public ActionResult AudioList()
+        {
+            return View();
+        }
+        public ActionResult VideoList()
+        {
+            return View();
+        }
+        public ActionResult ScVideo()
+        {
+            return View();
+        }
+        public ActionResult ScAudio()
+        {
+            return View();
+        }
+        public ActionResult ScAnli()
+        {
+            return View();
+        }
+        public ActionResult ScAnliCreate()
+        {
+            return View();
+        }
+        public ActionResult ScTuPian()
+        {
+            return View();
+        }
+        public ActionResult ScTuPianCreate()
+        {
+            return View();
+        }
+	}
+}
