aeb8995cc5eb28bd74d8d22dfe8791026c542e15
jorzen <jorzen2010@163.com>
2018-07-27 16:34:00 +0800

    ok

--- psycoderEntity/FensiUser.cs
+++ psycoderEntity/FensiUser.cs
@@ -31,9 +31,7 @@ namespace psycoderEntity
         public DateTime CreateTime { get; set; }
         [Display(Name = "是否启用")]
         public bool FensiStatus { get; set; }
-
-
-    
+   
     }
     public class FensiUser
     {
@@ -52,12 +50,14 @@ namespace psycoderEntity
         public string province { get; set; }
         [Display(Name = "城市")]
         public string city { get; set; }
-         [Display(Name = "语言")]
+        [Display(Name = "语言")]
         public string language { get; set; }
-         [Display(Name = "国家")]
+        [Display(Name = "国家")]
         public string country { get; set; }
         [Display(Name = "手机号")]
         public string FensiTelephone { get; set; }
+        [Display(Name = "密码")]
+        public string FensiPassword { get; set; }
         [Display(Name = "创建时间")]
         public DateTime CreateTime { get; set; }
         [Display(Name = "是否启用")]
@@ -70,6 +70,8 @@ namespace psycoderEntity
         public int Id { get; set; }
         [Display(Name = "邀请人")]
         public int SharePerson { get; set; }
+        [Display(Name = "密码")]
+        public string Password { get; set; }
         
     }
 }
