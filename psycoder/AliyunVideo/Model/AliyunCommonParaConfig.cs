4cebf796e41748af315bfa45487f5ded79511d80
jorzen <jorzen2010@163.com>
2020-04-28 11:27:56 +0800

    del_idandkey

--- psycoder/AliyunVideo/Model/AliyunCommonParaConfig.cs
+++ psycoder/AliyunVideo/Model/AliyunCommonParaConfig.cs
@@ -11,8 +11,8 @@ namespace AliyunVideo
         public const string ApiUrl = "vod.cn-shanghai.aliyuncs.com";
         public const string Format = "JSON";
         public const string Version = "2017-03-21";
-        public const string AccessKeyId = "LTAIHmoHhNfb1Eujxdc";
-        public const string AccessKeySecret = "afCSAjYmNuVcPLTLuwxCtwvu4VvYrrxds";
+        public const string AccessKeyId = "";
+        public const string AccessKeySecret = "";
       
         public const string SignatureVersion = "1.0";
 
